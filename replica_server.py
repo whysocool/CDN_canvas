19f951fc5d9bf9e37e6cc9a48d1e85fbaccbd26e
hhuangkelun@163.com <2643949372ha@gmail.com>
2024-11-01 23:41:06 -0400

    nothing new

--- replica_server.py
+++ replica_server.py
@@ -1,20 +1,27 @@
 # replica_server.py
+import sys
+
 from flask import Flask, request
 
 app = Flask(__name__)
 videos_store = []  # Store videos here
 
+
 @app.route('/store_video', methods=['POST'])
 def store_video():
     video = request.json.get('video')
     videos_store.append(video)
-    return "Video stored successfully."
+    return "Video stored successfully.", 200
+
 
 @app.route('/get_video/<video_name>', methods=['GET'])
 def get_video(video_name):
     if video_name in videos_store:
-        return f"Streaming video: {video_name}"
+        return f"Streaming video: {video_name}", 200
     return "Video not found.", 404
 
+
 if __name__ == '__main__':
-    app.run(port=5001)  # Change the port to 5002 and 5003 for other instances
+    # Use the port specified as a command-line argument, or default to 5001
+    port = int(sys.argv[1]) if len(sys.argv) > 1 else 5001
+    app.run(port=port)
